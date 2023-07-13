839391751160d910c053c0febfc1ef4e01d57f68
WhiteLeaf03 <2876202234@qq.com>
2023-07-13 15:12:33 +0800

    fix

--- src/main/java/top/whiteleaf03/api/InterfaceApplication.java
+++ src/main/java/top/whiteleaf03/api/InterfaceApplication.java
@@ -2,13 +2,11 @@ package top.whiteleaf03.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author WhiteLeaf03
  */
 @SpringBootApplication
-@EnableDiscoveryClient
 public class InterfaceApplication {
     public static void main(String[] args) {
         SpringApplication.run(InterfaceApplication.class, args);
